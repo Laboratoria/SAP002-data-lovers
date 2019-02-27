ec1391e3b842dcfc0da012fbec14475b55ae735b
janarf <janarf@hotmail.com>
2019-02-27 15:56:51 -0300

    ordenaçao sem tirar o filtro

--- src/main.js
+++ src/main.js
@@ -1,25 +1,26 @@
 window.onload = function() {
     // alert("carregou");
-    showPokemons();
+    showPokemons(getPokemons());
 
 };
+let pokemons = getPokemons()
+let selectedPokemons = pokemons
 
 function getPokemons() {
     return POKEMON["pokemon"]
 }
 
-let pokemons = getPokemons()
 const dropDownFilter = document.getElementById("dropdown-type")
 
 dropDownFilter.addEventListener('change',
     function() {
-
         let type = dropDownFilter.value
+        pokemons = getPokemons()
         console.log(type)
         if (type != "All") {
             pokemons = pokemons.filter(pokemon => pokemon["type"].includes(type))
         }
-        showPokemons()
+        showPokemons(pokemons)
     }
 )
 const dropDownOrder = document.getElementById("dropdown-order")
@@ -29,23 +30,23 @@ dropDownOrder.addEventListener('change',
         let order = dropDownOrder.value
         switch (order) {
             case "pokedex-num":
-                showPokemons()
+                showPokemons(pokemons)
                 break
             case "height":
                 pokemons = pokemons.sort(compareByHeight)
-                showPokemons()
+                showPokemons(pokemons)
                 break
             case "weight":
                 pokemons = pokemons.sort(compareByWeight)
-                showPokemons()
+                showPokemons(pokemons)
                 break
             case "spawn_chance":
                 pokemons = pokemons.sort(compareBySpawnChance)
-                showPokemons()
+                showPokemons(pokemons)
                 break
             case "alpha":
                 pokemons = pokemons.sort(compareByName)
-                showPokemons()
+                showPokemons(pokemons)
                 break
 
         }
@@ -74,7 +75,7 @@ function compareByPokedexNum(a, b) {
     return Number(a["num"]) - Number(b["num"])
 }
 
-function showPokemons() {
+function showPokemons(pokemons) {
     const pokemonDiv = document.getElementById("pokemons-div")
     pokemonDiv.innerHTML = `
     ${pokemons.map((pokemon)=> `
@@ -84,5 +85,5 @@ function showPokemons() {
          </figure>
       `).join("")}
   `
-  pokemons = getPokemons()
+
 }
