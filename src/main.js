64e4477b09ced5a1695e19414b95a72bef13d2d3
marinakeu <marina_keunecke@hotmail.com>
2019-02-27 11:04:39 -0300

    alteração no card - inclui todas as informações corretamente

--- src/main.js
+++ src/main.js
@@ -1,35 +1,29 @@
-window.onload = function(){
+window.onload = function () {
   showPokemons();
 };
 
 function getPokemons() {
-    return POKEMON["pokemon"];
-}
-
-function getType{
-  let getPokemons = getPokemons();
-  return getPokemons.
+  return POKEMON["pokemon"];
 }
 
 function showPokemons() {
-    let pokemonDiv = document.getElementById("pokemons-div")
+  let pokemonDiv = document.getElementById("pokemons-div")
 
-    pokemonDiv.innerHTML =`
+  pokemonDiv.innerHTML = `
     ${getPokemons().map((pokemon) => `
     <div>
         <img src="${pokemon["img"]}"
         <h3>${pokemon["name"]}</h3>
         <p>${pokemon["num"]}</p>
-        <div>${pokemon["type"].map(type) => `
-          <p>${type}</p>`}
-          <p>Poison</p>
+        <div>${pokemon["type"].map((type) => `
+        <p>${type}</p>`).join('')}
         </div>
-        <p>Candy: 25</p>
-        <p>Spawn-Chance 69%</p>
-        <p>Spawn-Time 20:00</p>
+          <p>Evolve: ${pokemon["candy_count"]} candy</p>       
+        <p>Spawn-Chance: ${pokemon["spawn_chance"]}%</p>
+        <p>Spawn-Time: ${pokemon["spawn_time"]}</p>
       </div>
         
-      `)}
+      `).join('')}
       `
 }
 
