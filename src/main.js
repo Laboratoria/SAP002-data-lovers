0414dd2481e495dc039d8d8648fa92070919418e
carlareginam10 <carlareginam10@gmail.com>
2019-02-27 17:32:03 -0300

    atualização da função show e CSS

--- src/main.js
+++ src/main.js
@@ -2,68 +2,95 @@
  let dropMenu = document.querySelector(".drop-menu");
  dropMenu.addEventListener("change", showChampions);
  
-//----------------------FUNÇÃO FUNCIONANDO ----------------------------------
+// //----------------------FUNÇÃO FUNCIONANDO ----------------------------------
 
-function showChampions(){
-    let typeChampion = dropMenu.value;    
-    let type;
-    let listChampions = document.querySelector(".show-champions"); 
-    //let championName = document.querySelector(".champion-name");
-    listChampions.innerHTML ="";
-        for (let champion in LOL.data) {
-            for(i in LOL.data[champion]["tags"]){
-                type = LOL.data[champion]["tags"][i]
-                    if( typeChampion === type){
-                    let img = document.createElement("img");
-                    img.src = LOL.data[champion]["splash"];
-                    listChampions.appendChild(img);
+// function showChampions(){
+//     let typeChampion = dropMenu.value;    
+//     let type;
+//     let listChampions = document.querySelector(".show-champions"); 
+//     let championName = document.querySelector(".champion-name")
 
-                    //---ALGO DE ERRADO NÃO ESTÁ CERTO! =( ----
-                    // ---NÃO ESTÁ BUSCANDO O NOME---
-                    // let name = document.createElement("h1");
-                    // name = LOL.data[champion]["name"];
-                    // championName.appendChild(name);
-                    
-                }          
-            }
+//         listChampions.innerHTML = ""
+//         for (let champion in LOL.data) {
+//             for(i in LOL.data[champion]["tags"]){
+//                 type = LOL.data[champion]["tags"][i]
+//                 if( typeChampion === type){
+//                     let img = document.createElement("img");
+//                     img.src = LOL.data[champion]["splash"];
+//                     listChampions.appendChild(img);
+                           
+//                     //---ALGO DE ERRADO NÃO ESTÁ CERTO! =( ----
+//                     // ---NÃO ESTÁ BUSCANDO O NOME---
+//                     // for(i in LOL.data[champion]["name"])   {
+//                     //     let name = document.createElement('h1');
+//                     //     name = LOL.data[champion]["name"];
+//                     //     championName.appendChild(name);
+//                 }
+
+                
     
-        }
+                
+
+//             }
+    
+//         }
  
-};
+// };
 
 // -------- NÃO ROLOU PQ A ESTRUTURA DO OBJ É DIFERENTE, NÃO ACEITA MAP NEM FILTER-----
 
-// window.onload = function() {
-//     // alert("carregou");
-//     showChampions();
-  
-//   };
+ 
+// function objToArray(obj){
+//     let result = [];
+//     for (key of Object.keys(obj)){
+//         result.push([key, obj[key]])
+//     }
+//     return result
 
-// function getChampions(){
-//     return LOL.data;
 // }
-// console.log(getChampions());
+
+
   
-// function showChampions(){
+  
+
+  
 
    
-//     let listChampions = document.querySelector(".show-champions");     
-//     listChampions.innerHTML=`
-//        ${getChampions().map((champion) =>`
-//           <section class="champions-container">
-//               <img src="${champion["splash"]}"class="show-champions"/>
-//               <section class="champion-img">
-//                 <h1 class="champion-name">${champion["name"]}</h1>
-//               </section>    
-//           </section>
-//     `).join("")}
-//     `
-// }
-  
+       
+    
+
+function showChampions(){
+    clearResult();
+    let typeChampion = dropMenu.value;    
+    let type;   
+    let listChampions = document.querySelector(".show-champions");     
+    
+    for (let champion in LOL.data) {
+        for(i in LOL.data[champion]["tags"]){
+            type = LOL.data[champion]["tags"][i]
+            if( typeChampion === type){ 
+
+                listChampions.innerHTML += `
+                <section class="champions-container">
+                    <div  class="show-champions">
+                        <div class='card'>
+                            <img src="${LOL.data[champion]['splash']}" class='champions-img' />
+                            <div class="tex-name">
+                                <h2 class="champion-name">${LOL.data[champion]['name']}</h2>
+                                <h1 class="champion-name">${LOL.data[champion]['tags']}</h1>
+                            </div>
+                        </div>
+                    </div>
+                 </section>
+   
+                `
+            }
+        }
+    }
+}
 
 
 
-
-
-
-
+function clearResult(){
+    document.querySelector(".show-champions").innerHTML = ""; 
+  }
