c85a8bae010be5bdfea62dd6439bbe1d09dfc767
Ana-Biscalchin <anacarolinabis@gmail.com>
2019-02-27 13:38:59 -0300

    html pronto e js andando, já puxa os enunciados

--- src/main.js
+++ src/main.js
@@ -1,2 +1,37 @@
-let teste = WORLDBANK["PER"]["indicators"]["countryName"];
-console.log(teste);
+const peruIndicators = WORLDBANK.PER.indicators;
+document.getElementById("peru").addEventListener("click", selectStatement);
+
+
+
+function selectStatement() {
+    document.getElementById("select-statement").innerHTML = `     
+    ${peruIndicators.map((element)=>  `<option> ${element.indicatorName} </option>`).join("")
+    }`
+
+};
+
+
+let statement = document.getElementById("view").addEventListener("click", capture() {
+    aquilo que estiver selecionado no select-statement.value
+    
+}   )
+
+console.log(statement)
+
+
+
+
+
+
+// let dataYear =  WORLDBANK.PER.indicators;
+// document.getElementById("select-year").innerHTML = `     
+//     ${peruIndicators.map((element)=>  `<div> ${element["data"]["2002"]} </div>`).join("")
+//     }`
+
+// let statement = selectStatement()
+
+
+// let newArray = peruIndicators.filter(element=> element.indicatorName === "Empleo de tiempo parcial, mujeres (% del total de mujeres empleadas)" )
+
+
+// console.table(newArray)
