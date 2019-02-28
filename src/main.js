5afb76d9989ebb04256f282e79678c43af7c7708
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-02-28 17:24:03 -0300

    manter master intacta para iniciar branch

--- src/main.js
+++ src/main.js
@@ -19,3 +19,15 @@ function showIndicadores(){
       <p>${indicador["indicators"][0].data[2015]}</p>
     </div>`).join('')}`
 }
+
+
+// VIOLENCIA
+// INDICADORES 
+
+// // Mujeres que creen que está justificado que un marido golpee a su esposa (cualquiera de las cinco razones) (%)
+
+
+// Proporción de mujeres víctimas de violencia física o sexual en los últimos 12 meses (% de mujeres de entre 15 y 49 años)
+
+// Dados somente em PERU - Lembrar ideia dashboars para este tema
+
