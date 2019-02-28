27db8344dbec13b3262438dfa6145538f26d7748
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-02-28 17:29:56 -0300

    insere coments em js para iniciar filtro indicadores e série temporal do recorte

--- src/main.js
+++ src/main.js
@@ -21,13 +21,25 @@ function showIndicadores(){
 }
 
 
-// VIOLENCIA
-// INDICADORES 
-
-// // Mujeres que creen que está justificado que un marido golpee a su esposa (cualquiera de las cinco razones) (%)
 
 
-// Proporción de mujeres víctimas de violencia física o sexual en los últimos 12 meses (% de mujeres de entre 15 y 49 años)
+/*EDUCACAO
+INDICADORES 
+SERIE TEMPORAL : 2013 até 2015
 
-// Dados somente em PERU - Lembrar ideia dashboars para este tema
+Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)
+var educaFirstTopic = 
+Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)
+var educaScndTopic =
+Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)
+var educaThirdTopic =
+
+
+VIOLENCIA
+INDICADORES 
+SERIE TEMPORAL : 2013 até 2015
+
+Mujeres que creen que está justificado que un marido golpee a su esposa (cualquiera de las cinco razones) (%)
+Proporción de mujeres víctimas de violencia física o sexual en los últimos 12 meses (% de mujeres de entre 15 y 49 años)
+Dados somente em PERU - Lembrar ideia dashboars para este tema*/
 
