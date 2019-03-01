0121a9f921a26a079d4c01e29f7f77483778adb0
alinemarjorie <aline.marjorie.af@gmail.com>
2019-03-01 16:52:57 -0300

    detalhes de pokemons finalizados, CSS iniciado

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 const selectTypeElement = document.getElementById("select-type");
-let filteredPokemon;
 
 selectTypeElement.addEventListener("change", typeFilter);
 
-function showPokemons(){
+function showPokemons(filteredPokemon){
     document.getElementById("pokemons").innerHTML = `${filteredPokemon.map(pokemon => `
-        <div class="each_pokemon">
+        <div class="each-pokemon">
         <img src="${pokemon.img}" class="pokemon-img"/>
         <div class="text-name">
             <h3 class="pokemon-name">${pokemon.name}</h3>
@@ -17,54 +16,49 @@ function showPokemons(){
 `}
 
 function typeFilter(){
-    filteredPokemon = POKEMON.pokemon.filter(pokemon => {
+    let filteredPokemon = POKEMON.pokemon.filter(pokemon => {
     return (pokemon.type.includes(selectTypeElement.value));
 });
-    showPokemons();
+    showPokemons(filteredPokemon);
 }
   
 const selectWeaknessesElement = document.getElementById("select-weaknesses");
 selectWeaknessesElement.addEventListener("change", weaknessesFilter);
 
 function weaknessesFilter(){
-    filteredPokemon = POKEMON.pokemon.filter(pokemon => {
+    let filteredPokemon = POKEMON.pokemon.filter(pokemon => {
         return (pokemon.weaknesses.includes(selectWeaknessesElement.value));
     })
-    showPokemons();
+    showPokemons(filteredPokemon);
 }
 
-let detailsPokemon;
 const pokeSearchElement = document.getElementById("poke-search");
-const pokeButtonElement = document.getElementById("poke-button");
+pokeSearchElement.addEventListener("keyup", pokemonSearch);
 
-pokeButtonElement.addEventListener("click",pokemonSearch());
-
-function pokemonSearch() {
-    detailsPokemon = POKEMON.pokemon.filter(pokemon => {
-    return (pokemon.name.includes(pokeSearchElement.value));
-})
-    showFullPokemon;
+function pokemonSearch() { 
+    let searchUpper = pokeSearchElement.value.toUpperCase();    
+    let detailsPokemon = POKEMON.pokemon.filter(pokemon => {
+        let nameUpper = pokemon.name.toUpperCase();
+        return (nameUpper.includes(searchUpper));
+    })
+    showFullPokemon(detailsPokemon);
 }
 
-function showFullPokemon() {
+function showFullPokemon(detailsPokemon) {
     document.getElementById("pokemons").innerHTML = `${detailsPokemon.map(pokemon => `
-    <div class="each_pokemon">
+    <div class="each-full-pokemon">
     <img src="${pokemon.img}" class="pokemon-img"/>
     <div class="text-name">
         <h3 class="pokemon-number">${pokemon.num}</h3>
         <h3 class="pokemon-name">${pokemon.name}</h3>
+        <p><strong>Tipo:</strong> ${pokemon.type}</p>
+        <p><strong>Fraquezas:</strong> ${pokemon.weaknesses}</p>
+        <p><strong>Horário:</strong> ${pokemon.spawn_time}</p>     
+        <p><strong>Altura:</strong> ${pokemon.height}</p>
+        <p><strong>Peso:</strong> ${pokemon.weight}</p>
     </div>
     </div>
 `
 ).join('')
 }
 `}
-
-
-
-
-
-
-
-
-
