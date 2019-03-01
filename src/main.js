62ee5ca2ab1cf708c5a82b9c0915b2613fc46614
JaquelineGDS <jaqueline_gd@outlook.com>
2019-03-01 15:17:34 -0300

    alteração funcao showStrengths

--- src/main.js
+++ src/main.js
@@ -1,152 +1,109 @@
-<<<<<<< HEAD
-/*window.onload = function () {
-  showChampions();
-}; */
- 
+/window.onload = function() {
+  //alert("carregou");   
+  showAllChampions();
+}; 
+
+
 let dropMenu = document.querySelector(".drop-menu");
 dropMenu.addEventListener("change", showChampions);
 
-//----------------------FUNÇÃO FUNCIONANDO ----------------------------------
-function getProducts() {
- return LOL["data"];
+// let dropMenuStrengths = document.querySelector(".drop-menu-strengths");
+// dropMenuStrengths.addEventListener("change", showStrengths);
+
+
+function showAllChampions(){       
+  let listChampions = document.querySelector(".show-champions");     
+  for (let champion in LOL.data) {
+       listChampions.innerHTML += `
+       <section class="champions-container">
+          <div  class="show-champions">
+              <div class='card card-color'>
+                  <img src="${LOL.data[champion]['splash']}" class='champions-img' />
+                  <div class="tex-name">
+                      <h2 class="champion-name">${LOL.data[champion]['name']}</h2>
+                      <h4 class="champion-history"><u>History</u> >>></h4>
+                              
+                  </div>
+              </div>
+          </div>
+       </section>
+ 
+      `
+  }
+ 
 }
 
 
 function showChampions(){
- clearEesult();
-   let typeChampion = dropMenu.value;    
-   let type;
-   let listChampions = document.querySelector(".show-champions"); 
-   
-   /*listChampions.innerHTML =   `
-    ${for(let champion in LOL.data){ 
-             for(let i in LOL.data[champion]["tags"]){
-                type = LOL.data[champion]["tags"][i];
-                console.log(type);
-                   if( typeChampion === type){  `
-                   
-                   <div class="champions-container">
-                     <img src="${LOL.data['champion']['splash']}" />
-                   </div>
-                   `}
-             }
-       }
-       
-     .join("")}
-     `
-} */
-
-  for(let champion in LOL.data){ 
-     for(let i in LOL.data[champion]["tags"]){
-         type = LOL.data[champion]["tags"][i];
-           if(typeChampion === type){ 
- 
-             /*  listChampions.innerHTML += 
-                "<div class='champions-container'>" 
-                + "<img src=" + LOL.data[champion]['splash'] + " />"
-                + "<div class='champion-name'>" 
-                + "<h1 class='champion-name'>" + LOL.data[champion]['name'] + " </h1>"
-                + "</div>" 
-                + "</div>" */
-
-            listChampions.innerHTML += `
-            <div class='show-champions'>
-                 <img src="${LOL.data[champion]['splash']}" />
-               <div class="champion-name">
-                 <h1 class="nome-campeoes">${LOL.data[champion]['name']}</h1>
-               </div>
-             </div>
-
-            `
-
-            }
-     }
-   }
+  clearResult();
+  let typeChampion = dropMenu.value;    
+  let type;   
+  let listChampions = document.querySelector(".show-champions");     
   
+  for (let champion in LOL.data) {
+      for(i in LOL.data[champion]["tags"]){
+          type = LOL.data[champion]["tags"][i]
+          if( typeChampion === type){ 
+
+              listChampions.innerHTML += `
+             
+              <section class="champions-container">
+                  <div  class="show-champions">
+                      <div class='card card-color'>
+                          <img src="${LOL.data[champion]['splash']}" class='champions-img' />
+                          <div class="tex-name">
+                              <h2 class="champion-name">${LOL.data[champion]['name']}</h2>
+                              <h1 class="champion-name">${LOL.data[champion]['tags'].join(' - ')}</h1>
+                          </div>
+                      </div>
+                  </div>
+               </section>
+ 
+              ` 
+          }
+      }
+  }
 }
 
-function clearEesult(){
- document.querySelector(".show-champions").innerHTML = ""; 
-}
-/*
-
-function showChampions(){
-   for (let champion in LOL.data){
-       let listChampions = document.querySelector(".show-champions"); 
-        listChampions.innerHTML += 
-            "<div class='champions-container'>" 
-            + "<img src=" + LOL.data[champion]['splash'] + " />"
-            + "</div>"  
-     }
-}*/
-=======
-
- let dropMenu = document.querySelector(".drop-menu");
- dropMenu.addEventListener("change", showChampions);
- 
-//----------------------FUNÇÃO FUNCIONANDO ----------------------------------
-
-function showChampions(){
-    let typeChampion = dropMenu.value;    
-    let type;
-    let listChampions = document.querySelector(".show-champions"); 
-    //let championName = document.querySelector(".champion-name");
-    listChampions.innerHTML ="";
-        for (let champion in LOL.data) {
-            for(i in LOL.data[champion]["tags"]){
-                type = LOL.data[champion]["tags"][i]
-                    if( typeChampion === type){
-                    let img = document.createElement("img");
-                    img.src = LOL.data[champion]["splash"];
-                    listChampions.appendChild(img);
-
-                    //---ALGO DE ERRADO NÃO ESTÁ CERTO! =( ----
-                    // ---NÃO ESTÁ BUSCANDO O NOME---
-                    // let name = document.createElement("h1");
-                    // name = LOL.data[champion]["name"];
-                    // championName.appendChild(name);
-                    
-                }          
-            }
-    
-        }
- 
-};
-
-// -------- NÃO ROLOU PQ A ESTRUTURA DO OBJ É DIFERENTE, NÃO ACEITA MAP NEM FILTER-----
-
-// window.onload = function() {
-//     // alert("carregou");
-//     showChampions();
+// function showStrengths(){
+//     clearResult();
+//     let typeStrengths = dropMenuStrengths.value;    
+//     let type;   
+//     let key;
+//     let listStrengths = document.querySelector(".show-champions");     
   
-//   };
+//     for (let champion in LOL.data) {
+//         for(i in LOL.data[champion]["info"]){
+//             type = Object.keys(LOL.data[champion]['info'])
+//             console.log("o que tem em type"+type)
+          
+//             if( typeStrengths === type){ 
+//                 //filtrar quem é o tem o maior valor do tipo selecionado
 
-// function getChampions(){
-//     return LOL.data;
+//                 listStrengths.innerHTML += `
+               
+//                 <section class="champions-container">
+//                     <div  class="show-champions">
+//                         <div class='card card-color'>
+//                             <img src="${LOL.data[champion]['splash']}" class='champions-img' />
+//                             <div class="tex-name">
+//                                 <h2 class="champion-name">${LOL.data[champion]['name']}</h2>
+//                                 <h1 class="champion-name">${LOL.data[champion]['info']}</h1>
+//                             </div>
+//                         </div>
+//                     </div>
+//                  </section>
+ 
+//                 `
+//             }
+//         }
+//     }
 // }
-// console.log(getChampions());
-  
-// function showChampions(){
-
-   
-//     let listChampions = document.querySelector(".show-champions");     
-//     listChampions.innerHTML=`
-//        ${getChampions().map((champion) =>`
-//           <section class="champions-container">
-//               <img src="${champion["splash"]}"class="show-champions"/>
-//               <section class="champion-img">
-//                 <h1 class="champion-name">${champion["name"]}</h1>
-//               </section>    
-//           </section>
-//     `).join("")}
-//     `
-// }
-  
 
 
+//console.log("objeto"+Object.keys(LOL.data.Aatrox.info))
 
 
-
-
-
->>>>>>> 01f1ddc360ff619d67176b3bb5e18a05dedc897a
+function clearResult(){
+  document.querySelector(".show-champions").innerHTML = ""; 
+}
