092535c5fd1c8bef9613d39be95fa4763bef3e9c
Ana-Biscalchin <anacarolinabis@gmail.com>
2019-03-04 15:32:43 -0300

    js filtra enunciado e quase publica os anos no botão

--- src/main.js
+++ src/main.js
@@ -2,20 +2,29 @@ const peruIndicators = WORLDBANK.PER.indicators;
 document.getElementById("peru").addEventListener("click", selectStatement);
 
 
+
 function selectStatement() {
     document.getElementById("select-statement").innerHTML = `     
   ${peruIndicators.map((element)=> `<option value="${element.indicatorName}"> ${element.indicatorName} </option>`).join("")
   }`;
-};
+  };
 
   document.getElementById("select-statement").addEventListener("change", function(event) {
-    let enunciado =  event.target.value
-    console.log(enunciado)
+  let enunciado =  event.target.value;
+  console.log(enunciado);
+
+  let position = peruIndicators.filter(position => (position.indicatorName === enunciado));
+    console.log(position)
+  
+  for (let i in position) {
+    let year = [];
+    year.push(Object.keys(position[i].data));
+      console.log(year)
+      return year
+      
+  }      
+
     
-    let position = peruIndicators.filter(item => (item.indicatorName === enunciado))
-      console.log(position)
-      
-      
-    document.getElementById("select-year").innerHTML = `${peruIndicators.map((element)=> `<option> ${position["data"]} </option>`).join("")
-  }`
-    })
+  
+  
+});
