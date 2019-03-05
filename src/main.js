bff4d9592d40bb827922483833af1b0fd3447f9c
Ana-Biscalchin <anacarolinabis@gmail.com>
2019-03-05 15:11:12 -0300

    js publica ano no select

--- src/main.js
+++ src/main.js
@@ -4,20 +4,18 @@ document.getElementById("peru").addEventListener("click", selectStatement);
 
 // publica no select de indicador os statements (enunciados)
 function selectStatement() {
-    document.getElementById("select-statement").innerHTML = `     
+    document.getElementById("select-statement").innerHTML = `
     ${peruIndicators.map((element)=> `<option value="${element.indicatorName}"> ${element.indicatorName} </option>`).join("")}`;
 };
 
 
-// deveria publicar os anos no select de ano --- não funciona, preciso da variável ano
-function selectYear(year) {
-  for (i in year) {
-    document.getElementById("select-year").innerHTML = ` <option> ${year} </option>` ;
-  }
+//publica o ano no select 
+function selectYear(array) {
+  document.getElementById("select-year").innerHTML = 
+  array.map((element) => `<option> ${element} </option>`).join("");
+
 }
-
-
-//capture o enunciado
+//captura o enunciado
 document.getElementById("select-statement").addEventListener("change", function(event) {
   let statement =  event.target.value;
     console.log(statement);
@@ -26,16 +24,10 @@ document.getElementById("select-statement").addEventListener("change", function(
     let position = peruIndicators.filter(position => (position.indicatorName === statement));
     console.log(position)
 
-  //mostre o que tem no data do objeto ao qual pertence o enunciado
-    let year = (position[0]["data"]);
-    console.table(year);
+  //mostre o que tem no data do objeto ao qual pertence o enunciado e chama a função que publica no select os anos
+  let year = Object.keys(position[0]["data"]) //uma array com  ["1960", "1961", "1962", "1963", ...]
+  console.table(year);
 
-  // execute a função que publica o ano no select
-    selectYear(year)
-
-  // deveria divulgar os resultados --- não funciona
-    for (i in year ){
-    document.getElementById("show-content").innerHTML = ` <div> ${year[i]} </div>`;
-  }
-  return year
+  selectYear(year)
+  
 });
