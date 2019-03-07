5857cd7b0388eee8c2d6e3f0c84e2a36bf97919a
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-03-07 14:08:25 -0300

    country-to-indicator

--- src/main.js
+++ src/main.js
@@ -1,174 +1,7 @@
-// window.onload = function(){
-//   showIndicadores();
-// }
-
-// function getData(){
-//   return Object.values(WORLDBANK)
-
-// }
-// console.log(getData())
-
-
-// function showIndicadores(){
-//   let productDiv = document.getElementById('indicators-div');
-//   productDiv.innerHTML = `
-//   ${getData().map((indicador) => `
-//     <div class="product-item">
-//       <p>${indicador["indicators"][74]["indicatorName"]}</p>
-//       <p>${indicador["indicators"][74]["countryName"]}</p>
-//       <p>${indicador["indicators"][74].data[2013]}</p>
-//       <p>${indicador["indicators"][74].data[2014]}</p>
-//       <p>${indicador["indicators"][74].data[2015]}</p>
-//     </div>`).join('')}`
-// }
-
-
-// const array = WORLDBANK.BRA.indicators
-
-// function indexIndicator(array, attr, value) {
-//     for(var i = 0; i < array.length; i++) {
-//         if(array[i][attr] === value) {
-//             return i;
-//         }
-//     }
-//     return -1;
-// }
-
-// const verData = WORLDBANK["BRA"]["indicators"][72]["data"][2015]
-// const nomeIndicador = WORLDBANK["MEX"]["indicators"][72]["indicatorName"]
-// console.log(verData)
-
-// console.log(indexIndicator(array, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)'))
-
-/*EDUCACAO
-INDICADORES 
-SERIE TEMPORAL : 2013 até 2015
-
-Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)
-var educaFirstTopic = 
-Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)
-var educaScndTopic =
-Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)
-var educaThirdTopic =
-
-
-VIOLENCIA
-INDICADORES 
-SERIE TEMPORAL : 2013 até 2015
-
-Mujeres que creen que está justificado que un marido golpee a su esposa (cualquiera de las cinco razones) (%)
-Proporción de mujeres víctimas de violencia física o sexual en los últimos 12 meses (% de mujeres de entre 15 y 49 años)
-Dados somente em PERU - Lembrar ideia dashboars para este tema*/
-
-
-// Branch: Section Indicator
-
-
-// function indicaEduca(array, attr) {
-//   for (let elem of arrayIndica) {
-//     console.log(array, attr);
-//     console.log(elem)
-//   }
-// }
-
-
-// function indexIndicator(array, attr, value) {
-//   for (var i = 0; i < array.length; i++) {
-//     if (array[i][attr] === value) {
-//       return i;
-//     }
-//   }
-//   return -1;
-// }
-
-// const arrayIndicaBra = WORLDBANK.BRA.indicators
-// const arrayIndicaPer = WORLDBANK.PER.indicators
-// const arrayIndicaMex = WORLDBANK.MEX.indicators
-// const arrayIndicaChi = WORLDBANK.CHL.indicators
-
-// function getEducaBra() {
-//   indexIndicator(arrayIndicaBra, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)')
-
-//   indexIndicator(arrayIndicaBra, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)')
-
-//   indexIndicator(arrayIndicaBra, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)')
-
-// }
-
-// function getEducaChl() {
-//   indexIndicator(arrayIndicaChi, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)')
-
-//   indexIndicator(arrayIndicaChi, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)')
-
-//   indexIndicator(arrayIndicaChi, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)')
-
-// }
-
-// function getEducaMex() {
-//   indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)')
-
-//   indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)')
-
-//   indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)')
-
-// }
-
-
-// function getEducaMex() {
-//   indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)')
-
-//   indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)')
-
-//   indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)')
-
-// }
-
-
-// console.log(indicaEducaABra, indicaEducaBBra, indicaEducaCBra)
-
-// const dataUm = WORLDBANK["BRA"]["indicators"][indicaEducaABra]["data"][2015]
-
-
-// console.log(dataUm)
-
-// // Solucionar - como pegar valor de radio button
-// document.getElementById('btnEscolha').addEventListener('click', getIndicaChoiche)
-
-// function getIndicaChoiche() {
-//   let indicador = document.forms[0];
-//   let txt = "";
-//   let i;
-//   for (i = 0; i < indicador.length; i++) {
-//     if (indicador[i].checked) {
-//       txt = txt + indicador[i].value + " ";
-//     }
-//   }
-//   return txt;
-// }
-
-// console.log(getIndicaChoiche())
-
-// function getMsgForm() {
-//   const novaMensagem = document.forms["tweetform"]["tweetmsg"].value;
-//   let tweet = document.createElement('p');
-//   tweet.innerHTML = novaMensagem;
-//   document.getElementById('mural').appendChild(horas);
-//   document.getElementById('mural').appendChild(tweet);
-//   document.forms["tweetform"]["tweetmsg"].value = '';
-//   btn.disabled = true;
-//   btn.style.cursor = 'not-allowed';
-//   event.preventDefault()
-// }
-
-// var template = $('#country-selection').html();
-
-// $('btn-topic-educa').click(function() {
-//     $('#topic').append(template);
-// });
-
 
 document.getElementById('btn-topic-educa').addEventListener('click', () => {
   selectCountryEduca();
+  cliqueBrasil()
 })
 
 document.getElementById('btn-topic-viol').addEventListener('click', () => {
@@ -179,13 +12,16 @@ document.getElementById('btn-topic-pop').addEventListener('click', () => {
   selectCountryPop();
 })
 
+
+// Cria templates da section country-selection
+
 function selectCountryEduca() {
   let productDiv = document.getElementById('country-selection');
   productDiv.innerHTML = `
   <div class="box-select">
 <div class="box-country">
   <div class="bra">
-    <a class="btn-coun*4try bra" href="#"><img class="img-bra" src="img/bra.png" alt=""></a>
+    <a id="btn-bra" class="btn-country bra" href="#"><img class="img-bra" src="img/bra.png" alt=""></a>
       <p class="p-country">Brasil</p>
 
 </div>
@@ -209,6 +45,7 @@ function selectCountryEduca() {
           <a href="#">INICIAR</a>
         </div>
 
+        
 
       </div>
       <article>
@@ -225,14 +62,13 @@ fugiat consequuntur perferendis explicabo, amet ipsa ex dolor vitae sed quasi to
       </article>`
 }
 
-
 function selectCountryViolence() {
   let productDiv = document.getElementById('country-selection');
   productDiv.innerHTML = `
   <div class="box-select">
 <div class="box-country">
   <div class="bra">
-    <a class="btn-coun*4try bra" href="#"><img class="img-bra" src="img/bra.png" alt=""></a>
+    <a class="btn-country bra" href="#"><img class="img-bra" src="img/bra.png" alt=""></a>
       <p class="p-country">Brasil</p>
 
 </div>
@@ -278,7 +114,7 @@ function selectCountryPop() {
   <div class="box-select">
 <div class="box-country">
   <div class="bra">
-    <a class="btn-coun*4try bra" href="#"><img class="img-bra" src="img/bra.png" alt=""></a>
+    <a class="btn-country bra" href="#"><img class="img-bra" src="img/bra.png" alt=""></a>
       <p class="p-country">Brasil</p>
 
 </div>
@@ -316,4 +152,92 @@ fugiat consequuntur perferendis explicabo, amet ipsa ex dolor vitae sed quasi to
         
         
       </article>`
+}
+
+
+// clicou brasil
+function indexIndicator(array, attr, value) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i][attr] === value) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+const arrayIndicaBra = WORLDBANK.BRA.indicators
+const arrayIndicaPer = WORLDBANK.PER.indicators
+const arrayIndicaMex = WORLDBANK.MEX.indicators
+const arrayIndicaChi = WORLDBANK.CHL.indicators
+
+function getEducaBra() {
+  return indexIndicator(arrayIndicaBra, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)')
+
+  indexIndicator(arrayIndicaBra, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)')
+
+  indexIndicator(arrayIndicaBra, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)')
+
+}
+
+console.log(getEducaBra())
+
+function getEducaChl() {
+  indexIndicator(arrayIndicaChi, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)')
+
+  indexIndicator(arrayIndicaChi, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)')
+
+  indexIndicator(arrayIndicaChi, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)')
+
+}
+
+function getEducaMex() {
+  indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)')
+
+  indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)')
+
+  indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)')
+
+}
+
+
+function getEducaPer() {
+  indexIndicator(arrayIndicaPer, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)')
+
+  indexIndicator(arrayIndicaPer, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)')
+
+  indexIndicator(arrayIndicaPer, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)')
+
+}
+
+
+let educaBraUm = indexIndicator(arrayIndicaBra, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)')
+
+let educaBraDois = indexIndicator(arrayIndicaBra, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)')
+
+let educaBraTres = indexIndicator(arrayIndicaBra, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)')
+
+console.log(educaBraUm, educaBraDois, educaBraTres)
+
+
+
+function cliqueBrasil() {
+  return WORLDBANK.BRA['indicators'][74].data[2013]  
+}
+
+
+
+console.log(WORLDBANK.BRA['indicators'][74])
+
+
+
+
+elements = document.getElementsByTagName("input");
+for(let el of elements) {
+  console.log("x");
+  el.addEventListener('click', printValue)
+}
+
+function printValue(event) {
+  valor = document.getElementById("formEduca").indicator.value;
+  console.log(valor);
 }
---
+++ src/testes.js
@@ -0,0 +1,187 @@
+// window.onload = function(){
+//   showIndicadores();
+// }
+
+// function getData(){
+//   return Object.values(WORLDBANK)
+
+// }
+// console.log(getData())
+
+
+// function showIndicadores(){
+//   let productDiv = document.getElementById('indicators-div');
+//   productDiv.innerHTML = `
+//   ${getData().map((indicador) => `
+//     <div class="product-item">
+//       <p>${indicador["indicators"][74]["indicatorName"]}</p>
+//       <p>${indicador["indicators"][74]["countryName"]}</p>
+//       <p>${indicador["indicators"][74].data[2013]}</p>
+//       <p>${indicador["indicators"][74].data[2014]}</p>
+//       <p>${indicador["indicators"][74].data[2015]}</p>
+//     </div>`).join('')}`
+// }
+
+
+// const array = WORLDBANK.BRA.indicators
+
+// function indexIndicator(array, attr, value) {
+//     for(var i = 0; i < array.length; i++) {
+//         if(array[i][attr] === value) {
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+
+// const verData = WORLDBANK["BRA"]["indicators"][72]["data"][2015]
+// const nomeIndicador = WORLDBANK["MEX"]["indicators"][72]["indicatorName"]
+// console.log(verData)
+
+// console.log(indexIndicator(array, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)'))
+
+/*EDUCACAO
+INDICADORES 
+SERIE TEMPORAL : 2013 até 2015
+
+Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)
+var educaFirstTopic = 
+Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)
+var educaScndTopic =
+Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)
+var educaThirdTopic =
+
+
+VIOLENCIA
+INDICADORES 
+SERIE TEMPORAL : 2013 até 2015
+
+Mujeres que creen que está justificado que un marido golpee a su esposa (cualquiera de las cinco razones) (%)
+Proporción de mujeres víctimas de violencia física o sexual en los últimos 12 meses (% de mujeres de entre 15 y 49 años)
+Dados somente em PERU - Lembrar ideia dashboars para este tema*/
+
+
+// Branch: Section Indicator
+
+
+// function indicaEduca(array, attr) {
+//   for (let elem of arrayIndica) {
+//     console.log(array, attr);
+//     console.log(elem)
+//   }
+// }
+
+
+// function indexIndicator(array, attr, value) {
+//   for (var i = 0; i < array.length; i++) {
+//     if (array[i][attr] === value) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+
+// const arrayIndicaBra = WORLDBANK.BRA.indicators
+// const arrayIndicaPer = WORLDBANK.PER.indicators
+// const arrayIndicaMex = WORLDBANK.MEX.indicators
+// const arrayIndicaChi = WORLDBANK.CHL.indicators
+
+// function getEducaBra() {
+//   indexIndicator(arrayIndicaBra, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)')
+
+//   indexIndicator(arrayIndicaBra, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)')
+
+//   indexIndicator(arrayIndicaBra, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)')
+
+// }
+
+// function getEducaChl() {
+//   indexIndicator(arrayIndicaChi, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)')
+
+//   indexIndicator(arrayIndicaChi, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)')
+
+//   indexIndicator(arrayIndicaChi, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)')
+
+// }
+
+// function getEducaMex() {
+//   indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)')
+
+//   indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)')
+
+//   indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)')
+
+// }
+
+
+// function getEducaMex() {
+//   indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación primaria (%)')
+
+//   indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)')
+
+//   indexIndicator(arrayIndicaMex, 'indicatorName', 'Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)')
+
+// }
+
+
+// console.log(indicaEducaABra, indicaEducaBBra, indicaEducaCBra)
+
+// const dataUm = WORLDBANK["BRA"]["indicators"][indicaEducaABra]["data"][2015]
+
+
+// console.log(dataUm)
+
+// // Solucionar - como pegar valor de radio button
+// document.getElementById('btnEscolha').addEventListener('click', getIndicaChoiche)
+
+// function getIndicaChoiche() {
+//   let indicador = document.forms[0];
+//   let txt = "";
+//   let i;
+//   for (i = 0; i < indicador.length; i++) {
+//     if (indicador[i].checked) {
+//       txt = txt + indicador[i].value + " ";
+//     }
+//   }
+//   return txt;
+// }
+
+// console.log(getIndicaChoiche())
+
+// function getMsgForm() {
+//   const novaMensagem = document.forms["tweetform"]["tweetmsg"].value;
+//   let tweet = document.createElement('p');
+//   tweet.innerHTML = novaMensagem;
+//   document.getElementById('mural').appendChild(horas);
+//   document.getElementById('mural').appendChild(tweet);
+//   document.forms["tweetform"]["tweetmsg"].value = '';
+//   btn.disabled = true;
+//   btn.style.cursor = 'not-allowed';
+//   event.preventDefault()
+// }
+
+// var template = $('#country-selection').html();
+
+// $('btn-topic-educa').click(function() {
+//     $('#topic').append(template);
+// });
+
+
+// tenta identificar valor do radio
+// values = [];
+
+// function addRecord() {
+//   var inp = document.getElementById('inputtext');
+//   values.push(inp.value);
+//   inp.value = "";  
+// }
+
+// function displayRecord() {
+//   document.getElementById("values").innerHTML = values.join(", ");
+// }
+
+
+// var form = document.getElementById('formEduca')
+// for (buttons of form) {
+//   console.log(buttons.checked = true)
+// }
