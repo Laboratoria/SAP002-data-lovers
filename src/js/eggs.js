fed1a27d928a755e58808140f83311cfc4b3ceff
Rafael Cerri <rafaelbcerri@hotmail.com>
2019-03-14 11:41:42 -0300

    Refactor showPokemonEggs function

--- src/js/eggs.js
+++ src/js/eggs.js
@@ -1,28 +1,23 @@
-fetch('./data/pokemon/pokemon.json').then(response => {
-    return response.json();
-}).then(data => {
-    function showPokemonEggs(){
-        let data2k = data["pokemon"].filter((pokemon) => (pokemon["egg"] === "2 km"));
-        let pokemon2k = document.getElementById("doisKm");
-        pokemon2k.innerHTML = `
-        ${data2k.map((pokemon) =>`
-        <img src="${pokemon["img"]}">
-        `).join("")}
-        `
-        let data5k = data["pokemon"].filter((pokemon) => (pokemon["egg"] === "5 km"));
-        let pokemon5k = document.getElementById("cincoKm");
-        pokemon5k.innerHTML = `
-        ${data5k.map((pokemon) =>`
-        <img src="${pokemon["img"]}">
-        `).join("")}
-        `
-        let data10k = data["pokemon"].filter((pokemon) => (pokemon["egg"] === "10 km"));
-        let pokemon10k = document.getElementById("dezKm");
-        pokemon10k.innerHTML = `
+fetch('./data/pokemon/pokemon.json').then(response => response.json()).then(data => showPokemonEggs(data))
+
+function showPokemonEggs(data){
+    const data2k = data["pokemon"].filter((pokemon) => (pokemon["egg"] === "2 km"));
+    const data5k = data["pokemon"].filter((pokemon) => (pokemon["egg"] === "5 km"));
+    const data10k = data["pokemon"].filter((pokemon) => (pokemon["egg"] === "10 km"));
+    
+    const pokemon2k = document.getElementById("doisKm");
+    const pokemon5k = document.getElementById("cincoKm");
+    const pokemon10k = document.getElementById("dezKm");
+    
+    pokemon2k.innerHTML = buildPokemonsImages(pokemon2k);
+    pokemon5k.innerHTML = buildPokemonsImages(pokemon5k);
+    pokemon10k.innerHTML = buildPokemonsImages(pokemon10k);
+}
+
+function buildPokemonsImages(pokemons) {
+    return `
         ${data10k.map((pokemon) =>`
-        <img src="${pokemon["img"]}">
+            <img src="${pokemon["img"]}">
         `).join("")}
-        `
-    }
-    showPokemonEggs()
-})
+    `;
+}
